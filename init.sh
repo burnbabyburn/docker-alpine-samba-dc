b86bd13eb088e4eb52216c58ba63ad3b65aaf4e3
0 <xyz258@abwesend.de>
2020-07-06 16:05:59 +0200

    posix

--- init.sh
+++ init.sh
@@ -60,7 +60,7 @@ appSetup () {
 	done
 	IFS=''
 	# If multi-site, we need to connect to the VPN before joining the domain
-	if [[ $(echo "$MULTISITE" | tr '[:upper:]' '[:lower:]') == "true" ]]; then
+	if [ $(echo "$MULTISITE" | tr '[:upper:]' '[:lower:]') = "true" ]; then
 		/usr/sbin/openvpn --config /docker.ovpn &
 		VPNPID=$!
 		echo "Sleeping 30s to ensure VPN connects ($VPNPID)";
@@ -68,19 +68,19 @@ appSetup () {
 	fi
 
 	# Set host ip option
-	if [[ "$RFC2307" == "true" ]]; then
+	if [ "$RFC2307" = "true" ]; then
 		RFC_OPTION="--use-rfc2307"
 	else
 		RFC_OPTION=""
 	fi
 	
-		if [[ "$HOSTIP" != "NONE" ]]; then
+		if [ "$HOSTIP" != "NONE" ]; then
 		HOSTIP_OPTION="--host-ip=${HOSTIP}"
 	else
 		HOSTIP_OPTION=""
 	fi
 	
-	if [[ "$DEBUG" == "true" ]]; then
+	if [ "$DEBUG" = "true" ]; then
 		SAMBA_DEBUG_OPTION="-d $DEBUGLEVEL"
 		SAMBADAEMON_DEBUG_OPTION="--debug-stderr -d $DEBUGLEVEL"
 		#NTP_DEBUG_OPTION="-D $DEBUGLEVEL"
@@ -90,11 +90,11 @@ appSetup () {
 		SAMBADAEMON_DEBUG_OPTION=""
 	fi
 
-	if [[ ! -d /etc/samba/external/ ]]; then
+	if [ ! -d /etc/samba/external/ ]; then
 		mkdir /etc/samba/external
 	fi
 
-	if [[ $(echo "$LOGS" | tr '[:upper:]' '[:lower:]') == "true" ]]; then
+	if [ $(echo "$LOGS" | tr '[:upper:]' '[:lower:]') = "true" ]; then
 	{
 		echo ""
 		echo "[logging]"
@@ -105,12 +105,12 @@ appSetup () {
 	fi
 
 	# If the finished file isn't there, this is brand new, we're not just moving to a new container
-	if [[ ! -f /etc/samba/external/smb.conf ]]; then
+	if [ ! -f /etc/samba/external/smb.conf ]; then
 		if [[ -f /etc/samba/smb.conf ]]; then
 			mv /etc/samba/smb.conf /etc/samba/smb.conf.orig
 		fi
 
-		if [[ $(echo "$JOIN" | tr '[:upper:]' '[:lower:]') == "true" ]]; then
+		if [ $(echo "$JOIN" | tr '[:upper:]' '[:lower:]') = "true" ]; then
 			if [[ ${JOINSITE} == "NONE" ]]; then
 				samba-tool domain join ${LDOMAIN} DC -U${URDOMAIN}\\${DOMAINUSER} --password=${DOMAINPASS} --dns-backend=SAMBA_INTERNAL ${SAMBA_DEBUG_OPTION}
 			else
@@ -212,7 +212,7 @@ username map = /etc/samba/user.map\
 				" /etc/samba/smb.conf
 		fi
 		
-		if [[ $(echo "$TLS" | tr '[:upper:]' '[:lower:]') == "true" ]]; then
+		if [ $(echo "$TLS" | tr '[:upper:]' '[:lower:]') = "true" ]; then
 #		openssl dhparam -out /var/lib/samba/private/tls/dh.key 2048 
 		sed -i "/\[global\]/a \
 tls enabled  = yes\\n\
@@ -233,7 +233,7 @@ reject md5 clients = yes\
 		
 
 		fi
-		if [[ $(echo "$MSCHAPV2" | tr '[:upper:]' '[:lower:]') == "true" ]]; then
+		if [ $(echo "$MSCHAPV2" | tr '[:upper:]' '[:lower:]') = "true" ]; then
 		sed -i "/\[global\]/a \
 ntlm auth = mschapv2-and-ntlmv2-only\
 		" /etc/samba/smb.conf
@@ -249,14 +249,14 @@ printcap name = /dev/null\\n\
 disable spoolss = yes\
 		" /etc/samba/smb.conf
 		
-		if [[ $(echo "$LOGS" | tr '[:upper:]' '[:lower:]') == "true" ]]; then
+		if [ $(echo "$LOGS" | tr '[:upper:]' '[:lower:]') = "true" ]; then
 			sed -i "/\[global\]/a \
 log file = /var/log/samba/%m.log\\n\
 max log size = 10000\\n\
 log level = 1\
 			" /etc/samba/smb.conf
 		fi
-		if [[ $(echo "$INSECURELDAP" | tr '[:upper:]' '[:lower:]') == "true" ]]; then
+		if [ $(echo "$INSECURELDAP" | tr '[:upper:]' '[:lower:]') = "true" ]; then
 			sed -i "/\[global\]/a \
 ldap server require strong auth = no\
 			" /etc/samba/smb.conf
@@ -331,7 +331,7 @@ appStart () {
 
 case "$1" in
 	start)
-		if [[ -f /etc/samba/external/smb.conf ]]; then
+		if [ -f /etc/samba/external/smb.conf ]; then
 			cp /etc/samba/external/smb.conf /etc/samba/smb.conf
 			appStart
 		else
